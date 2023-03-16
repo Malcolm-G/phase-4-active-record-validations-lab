b56ba39ebf59f9c0026f52208ab07d7a4fee3360
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-16 14:04:28 +0300

    completed task

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence:true, exclusion: { in: ['True Facts']}
+    validates :content, length:{minimum:250}
+    validates :summary, length:{maximum:250}
+    validates :category, acceptance:{ accept: ['Fiction', 'Non-Fiction'],message: "is not included in the list" } 
 end
